03590992e74f0de6f5c5ad3cc4b772c10596c7ad
dakshkaushik27 <95082216+dakshkaushik27@users.noreply.github.com>
2022-02-19 12:41:50 +0530

    Add files via upload

---
+++ flaskpractice/IdeaProjects/untitled2/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.Scanner;
+
+
+    }
+
+}
---
+++ flaskpractice/IdeaProjects/untitled3/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner ;
+import java.applet.*;
+public class Main {
+
+    public static void main(String[] args) {
+
+     int i,j;
+       i=10;
+        System.out.println(i);
+        Scanner s = new Scanner(System.in) ;
+        
+
+    }
+}
